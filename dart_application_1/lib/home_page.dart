01b3784eb8a15e04a2123467768dd4fa7428ac41
ThenukaW <98868025+ThenukaW@users.noreply.github.com>
2023-07-13 22:24:22 +0530

    first co

---
+++ dart_application_1/lib/home_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required String title});
+
+  @override
+  State<MyHomePage> createState() => _MYHomePageState();
+
+}
+
+class _MYHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: const Center(
+        child: Text('Hi'),
+      )
+    );
+  }
+}
